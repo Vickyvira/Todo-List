739522e4442f77a4717f7fe511243945d7e08f7f
Vicky Kumar Das <vk9793@gmail.com>
2021-08-27 11:35:54 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            this is an about components
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam est cum odit hic, magni, excepturi laboriosam, vero nisi sit quibusdam unde at ipsum odio commodi maxime dolore perferendis dignissimos tempora molestiae numquam eligendi reiciendis!</p>
+        </div>
+    )
+}
